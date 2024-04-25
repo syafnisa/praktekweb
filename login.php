58067356c378e610ddbdf5126a2d0a730e0a97a5
syafnisa <167904414+syafnisa@users.noreply.github.com>
2024-04-25 01:13:01 -0700

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 session_start();
 $email = $_POST['email'];
 $password = $_POST['password'];
-if($email == "idris@usm.ac.id" && $password == "123"){
+if($email == "idris@usm.ac.id" && $password == "123" && $umur >= 16 && $umur <= 44){
     // echo "<b>Login Berhasil</b>";
     $_SESSION['login'] = true;
     header('Location: http://localhost/praktekweb/dashboard.php');
